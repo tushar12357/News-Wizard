7d6b6f36a4e34660e94d8c4cab6338629ec2e302
tushar12357 <110051562+tushar12357@users.noreply.github.com>
2024-06-21 17:34:36 +0530

    Update News.js

--- src/components/News.js
+++ src/components/News.js
@@ -28,7 +28,7 @@ export class News extends Component {
     async componentDidMount(){
         // console.log("cdm")
         this.props.setProgress(10)
-        let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=813e47dc1ce041db8ebcb220ec6fb469&page=${this.state.page}&pageSize=${this.props.pageSize}`
+        let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=87518db39c3e4bc0b60fca7cca74daa9&page=${this.state.page}&pageSize=${this.props.pageSize}`
         let data=await fetch(url)
         let parsedData=await data.json()
         // console.log(parsedData)
@@ -66,7 +66,7 @@ export class News extends Component {
     // }
     fetchMoreData = async() => {
       this.setState({page:this.state.page+1})
-      let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=813e47dc1ce041db8ebcb220ec6fb469&page=${this.state.page}&pageSize=${this.props.pageSize}`
+      let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=87518db39c3e4bc0b60fca7cca74daa9&page=${this.state.page}&pageSize=${this.props.pageSize}`
         let data=await fetch(url)
         let parsedData=await data.json()
         // console.log(parsedData)
